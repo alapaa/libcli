fc2a2cdbae4839bb95148eb98b24aa343349c4ad
erik <erik@itancan.com>
2011-02-23 17:28:38 +0100

    Merged in stuff from libev echo server

--- clitest.c
+++ clitest.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <sys/types.h>
-#ifdef WIN32
-#include <winsock2.h>
-#include <windows.h>
-#else
+
 #include <netinet/in.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#endif
+
 #include <signal.h>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include "libcli.h"
+
+// Includes from unix echo server code
+#include <errno.h>
+#include <fcntl.h>
+
+#include <sys/un.h>
+
+#include <ev.h>
+
+#include "array-heap.h"
+// End includes from unix echo server code
 
 #define CLITEST_PORT                8000
 #define MODE_CONFIG_INT             10
@@ -23,44 +31,213 @@
 # define UNUSED(d) d
 #endif
 
+struct sock_ev_serv {
+    ev_io io;
+    int fd;
+    struct sockaddr_un socket;
+    int socket_len;
+    array clients;
+};
+
+struct sock_ev_client {
+    ev_io io;
+    int fd;
+    int index;
+    struct sock_ev_serv* server;
+
+    struct cli_def *cli;
+};
+
+int setnonblock(int fd);
+static void not_blocked(EV_P_ ev_periodic *w, int revents);
+
 unsigned int regular_count = 0;
 unsigned int debug_regular = 0;
+//------------
+// This callback is called when client data is available
+static void client_cb(EV_P_ ev_io *w, int revents) {
+  // a client has become readable
 
-#ifdef WIN32
-typedef int socklen_t;
+  struct sock_ev_client* client = (struct sock_ev_client*) w;
 
-int winsock_init()
-{
-    WORD wVersionRequested;
-    WSADATA wsaData;
-    int err;
+  cli_process_event(EV_P_ ev_io *w, int revents, struct cli_def *cli)
+  cli_process_event(w, revents, client->cli)
 
-    // Start up sockets
-    wVersionRequested = MAKEWORD(2, 2);
+  /* int n; */
+  /* char str[100] = ".\0"; */
 
-    err = WSAStartup(wVersionRequested, &wsaData);
-    if (err != 0)
-    {
-        // Tell the user that we could not find a usable WinSock DLL.
-        return 0;
-    }
+  /* printf("[r]"); */
+  /* n = recv(client->fd, str, 100, 0); */
+  /* if (n <= 0) { */
+  /*   if (0 == n) { */
+  /*     // an orderly disconnect */
+  /*     puts("orderly disconnect"); */
+  /*     ev_io_stop(EV_A_ &client->io); */
+  /*     close(client->fd); */
+  /*   }  else if (EAGAIN == errno) { */
+  /*     puts("should never get in this state with libev"); */
+  /*   } else { */
+  /*     perror("recv"); */
+  /*   } */
+  /*   return; */
+  /* } */
+  /* printf("socket client said: %s", str); */
 
-    /*
-     * Confirm that the WinSock DLL supports 2.2
-     * Note that if the DLL supports versions greater than 2.2 in addition to
-     * 2.2, it will still return 2.2 in wVersion since that is the version we
-     * requested.
-     * */
-    if (LOBYTE(wsaData.wVersion) != 2 || HIBYTE(wsaData.wVersion) != 2)
-    {
-        // Tell the user that we could not find a usable WinSock DLL.
-        WSACleanup();
-        return 0;
-    }
-    return 1;
+  /* // Assuming that whenever a client is readable, it is also writable ? */
+  /* if (send(client->fd, str, n, 0) < 0) { */
+  /*   perror("send"); */
+  /* } */
 }
-#endif
 
+inline static struct sock_ev_client* client_new(int fd) {
+  struct sock_ev_client* client;
+
+  client = realloc(NULL, sizeof(struct sock_ev_client));
+  client->fd = fd;
+  //client->server = server;
+  setnonblock(client->fd);
+  ev_io_init(&client->io, client_cb, client->fd, EV_READ);
+
+  return client;
+}
+
+// This callback is called when data is readable on the unix socket.
+static void server_cb(EV_P_ ev_io *w, int revents) {
+  puts("unix stream socket has become readable");
+
+  int client_fd;
+  struct sock_ev_client* client;
+
+  // since ev_io is the first member,
+  // watcher `w` has the address of the
+  // start of the sock_ev_serv struct
+  struct sock_ev_serv* server = (struct sock_ev_serv*) w;
+
+  while (1)
+  {
+    client_fd = accept(server->fd, NULL, NULL);
+    if( client_fd == -1 )
+    {
+      if( errno != EAGAIN && errno != EWOULDBLOCK )
+      {
+        g_warning("accept() failed errno=%i (%s)",  errno, strerror(errno));
+        exit(EXIT_FAILURE);
+      }
+      break;
+    }
+    puts("accepted a client");
+    client = client_new(client_fd);
+    client->server = server;
+    client->index = array_push(&server->clients, client);
+    ev_io_start(EV_A_ &client->io);
+  }
+}
+
+// Simply adds O_NONBLOCK to the file descriptor of choice
+int setnonblock(int fd)
+{
+  int flags;
+
+  flags = fcntl(fd, F_GETFL);
+  flags |= O_NONBLOCK;
+  return fcntl(fd, F_SETFL, flags);
+}
+
+int socket_init(struct sockaddr_un* socket_un, int max_queue) {
+  int fd;
+
+  // Setup a socket listener.
+
+  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+  {
+      perror("socket");
+      exit(EXIT_FAILURE);
+  }
+  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+
+  // Set it non-blocking
+  if (-1 == setnonblock(fd)) {
+    perror("echo server socket nonblock");
+    exit(EXIT_FAILURE);
+  }
+
+  // Set it as unix socket
+  socket_un->sun_family = AF_INET;
+  //strcpy(socket_un->sun_path, sock_path);
+
+  return fd;
+}
+
+int server_init(struct sock_ev_serv* server, int max_queue) {
+    server->fd = socket_init(&server->socket, max_queue);
+    //server->socket_len = sizeof(server->socket.sun_family) + strlen(server->socket.sun_path);
+    server->socket_len = sizeof(server->socket.sun_family));
+
+    array_init(&server->clients, 128);
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(CLITEST_PORT);
+
+    if (-1 == bind(server->fd, (struct sockaddr*) &server->socket, server->socket_len))
+    {
+      perror("echo server bind");
+      exit(EXIT_FAILURE);
+    }
+
+    if (-1 == listen(server->fd, max_queue)) {
+      perror("listen");
+      exit(EXIT_FAILURE);
+    }
+
+    printf("Listening on port %d\n", CLITEST_PORT);
+    return 0;
+}
+
+int main(void) {
+    int max_queue = 128;
+    struct sock_ev_serv server;
+    struct ev_periodic every_few_seconds;
+
+    int result = setup_cli();
+    if (result < 0) {
+        perror("CLI setup failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Create our single-loop for this single-thread application
+    EV_P  = ev_default_loop(0);
+
+    // Create unix socket in non-blocking fashion
+    //server_init(&server, "/tmp/libev-echo.sock", max_queue);
+    server_init(&server, max_queue);
+
+    // To be sure that we aren't actually blocking
+    ev_periodic_init(&every_few_seconds, not_blocked, 0, 5, 0);
+    ev_periodic_start(EV_A_ &every_few_seconds);
+
+    // Get notified whenever the socket is ready to read
+    ev_io_init(&server.io, server_cb, server.fd, EV_READ);
+    ev_io_start(EV_A_ &server.io);
+
+    // Run our loop, ostensibly forever
+    puts("tcp-socket-echo starting...\n");
+    ev_loop(EV_A_ 0);
+
+    // This point is only ever reached if the loop is manually exited
+    close(server.fd);
+    return EXIT_SUCCESS;
+}
+
+
+static void not_blocked(EV_P_ ev_periodic *w, int revents) {
+  puts("I'm not blocked");
+}
+
+
+//------------
 int cmd_test(struct cli_def *cli, char *command, char *argv[], int argc)
 {
     int i;
@@ -187,7 +364,7 @@ void pc(UNUSED(struct cli_def *cli), char *string)
     printf("%s\n", string);
 }
 
-int main()
+int setup_cli()
 {
     struct cli_command *c;
     struct cli_def *cli;
@@ -195,22 +372,15 @@ int main()
     struct sockaddr_in addr;
     int on = 1;
 
-#ifndef WIN32
+
     signal(SIGCHLD, SIG_IGN);
-#endif
-#ifdef WIN32
-    if (!winsock_init()) {
-        printf("Error initialising winsock\n");
-        return 1;
-    }
-#endif
 
     cli = cli_init();
     cli_set_banner(cli, "libcli test environment");
     cli_set_hostname(cli, "router");
-    cli_regular(cli, regular_callback);
-    cli_regular_interval(cli, 5); // Defaults to 1 second
-    cli_set_idle_timeout_callback(cli, 60, idle_timeout); // 60 second idle timeout
+    //cli_regular(cli, regular_callback);
+    //cli_regular_interval(cli, 5); // Defaults to 1 second
+    //cli_set_idle_timeout_callback(cli, 60, idle_timeout); // 60 second idle timeout
     cli_register_command(cli, NULL, "test", cmd_test, PRIVILEGE_UNPRIVILEGED,
         MODE_EXEC, NULL);
 
@@ -267,33 +437,16 @@ int main()
         }
     }
 
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        perror("socket");
-        return 1;
-    }
-    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    return 0;
+}
 
-    memset(&addr, 0, sizeof(addr));
-    addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    addr.sin_port = htons(CLITEST_PORT);
-    if (bind(s, (struct sockaddr *) &addr, sizeof(addr)) < 0)
-    {
-        perror("bind");
-        return 1;
-    }
+//-- old main
+int old_main()
+{
 
-    if (listen(s, 50) < 0)
-    {
-        perror("listen");
-        return 1;
-    }
 
-    printf("Listening on port %d\n", CLITEST_PORT);
     while ((x = accept(s, NULL, 0)))
     {
-#ifndef WIN32
         int pid = fork();
         if (pid < 0)
         {
@@ -316,11 +469,6 @@ int main()
         close(s);
         cli_loop(cli, x);
         exit(0);
-#else
-        cli_loop(cli, x);
-        shutdown(x, SD_BOTH);
-        close(x);
-#endif
     }
 
     cli_done(cli);
