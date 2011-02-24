1fb77764824b03cb832bd85d5a692f013172e0a7
erik <erik@itancan.com>
2011-02-24 09:14:42 +0100

    Added rest of changes to libev echo server

--- clitest.c
+++ clitest.c
@@ -56,117 +56,117 @@ unsigned int debug_regular = 0;
 //------------
 // This callback is called when client data is available
 static void client_cb(EV_P_ ev_io *w, int revents) {
-  // a client has become readable
+    // a client has become readable
 
-  struct sock_ev_client* client = (struct sock_ev_client*) w;
+    struct sock_ev_client* client = (struct sock_ev_client*) w;
 
-  cli_process_event(EV_P_ ev_io *w, int revents, struct cli_def *cli)
-  cli_process_event(w, revents, client->cli)
+    //cli_process_event(sock_ev_client *client, int revents)
+    cli_process_event(client, revents);
 
-  /* int n; */
-  /* char str[100] = ".\0"; */
+        /* int n; */
+        /* char str[100] = ".\0"; */
 
-  /* printf("[r]"); */
-  /* n = recv(client->fd, str, 100, 0); */
-  /* if (n <= 0) { */
-  /*   if (0 == n) { */
-  /*     // an orderly disconnect */
-  /*     puts("orderly disconnect"); */
-  /*     ev_io_stop(EV_A_ &client->io); */
-  /*     close(client->fd); */
-  /*   }  else if (EAGAIN == errno) { */
-  /*     puts("should never get in this state with libev"); */
-  /*   } else { */
-  /*     perror("recv"); */
-  /*   } */
-  /*   return; */
-  /* } */
-  /* printf("socket client said: %s", str); */
+        /* printf("[r]"); */
+        /* n = recv(client->fd, str, 100, 0); */
+        /* if (n <= 0) { */
+        /*   if (0 == n) { */
+        /*     // an orderly disconnect */
+        /*     puts("orderly disconnect"); */
+        /*     ev_io_stop(EV_A_ &client->io); */
+        /*     close(client->fd); */
+        /*   }  else if (EAGAIN == errno) { */
+        /*     puts("should never get in this state with libev"); */
+        /*   } else { */
+        /*     perror("recv"); */
+        /*   } */
+        /*   return; */
+        /* } */
+        /* printf("socket client said: %s", str); */
 
-  /* // Assuming that whenever a client is readable, it is also writable ? */
-  /* if (send(client->fd, str, n, 0) < 0) { */
-  /*   perror("send"); */
-  /* } */
-}
+        /* // Assuming that whenever a client is readable, it is also writable ? */
+        /* if (send(client->fd, str, n, 0) < 0) { */
+        /*   perror("send"); */
+        /* } */
+        }
 
 inline static struct sock_ev_client* client_new(int fd) {
-  struct sock_ev_client* client;
+    struct sock_ev_client* client;
 
-  client = realloc(NULL, sizeof(struct sock_ev_client));
-  client->fd = fd;
-  //client->server = server;
-  setnonblock(client->fd);
-  ev_io_init(&client->io, client_cb, client->fd, EV_READ);
+    client = realloc(NULL, sizeof(struct sock_ev_client));
+    client->fd = fd;
+    //client->server = server;
+    setnonblock(client->fd);
+    ev_io_init(&client->io, client_cb, client->fd, EV_READ);
 
-  return client;
+    return client;
 }
 
 // This callback is called when data is readable on the unix socket.
 static void server_cb(EV_P_ ev_io *w, int revents) {
-  puts("unix stream socket has become readable");
+    puts("unix stream socket has become readable");
 
-  int client_fd;
-  struct sock_ev_client* client;
+    int client_fd;
+    struct sock_ev_client* client;
 
-  // since ev_io is the first member,
-  // watcher `w` has the address of the
-  // start of the sock_ev_serv struct
-  struct sock_ev_serv* server = (struct sock_ev_serv*) w;
+    // since ev_io is the first member,
+    // watcher `w` has the address of the
+    // start of the sock_ev_serv struct
+    struct sock_ev_serv* server = (struct sock_ev_serv*) w;
 
-  while (1)
-  {
-    client_fd = accept(server->fd, NULL, NULL);
-    if( client_fd == -1 )
+    while (1)
     {
-      if( errno != EAGAIN && errno != EWOULDBLOCK )
-      {
-        g_warning("accept() failed errno=%i (%s)",  errno, strerror(errno));
-        exit(EXIT_FAILURE);
-      }
-      break;
+        client_fd = accept(server->fd, NULL, NULL);
+        if( client_fd == -1 )
+        {
+            if( errno != EAGAIN && errno != EWOULDBLOCK )
+            {
+                g_warning("accept() failed errno=%i (%s)",  errno, strerror(errno));
+                exit(EXIT_FAILURE);
+            }
+            break;
+        }
+        puts("accepted a client");
+        client = client_new(client_fd);
+        client->server = server;
+        client->index = array_push(&server->clients, client);
+        ev_io_start(EV_A_ &client->io);
     }
-    puts("accepted a client");
-    client = client_new(client_fd);
-    client->server = server;
-    client->index = array_push(&server->clients, client);
-    ev_io_start(EV_A_ &client->io);
-  }
 }
 
 // Simply adds O_NONBLOCK to the file descriptor of choice
 int setnonblock(int fd)
 {
-  int flags;
+    int flags;
 
-  flags = fcntl(fd, F_GETFL);
-  flags |= O_NONBLOCK;
-  return fcntl(fd, F_SETFL, flags);
+    flags = fcntl(fd, F_GETFL);
+    flags |= O_NONBLOCK;
+    return fcntl(fd, F_SETFL, flags);
 }
 
 int socket_init(struct sockaddr_un* socket_un, int max_queue) {
-  int fd;
+    int fd;
 
-  // Setup a socket listener.
+    // Setup a socket listener.
 
-  if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-  {
-      perror("socket");
-      exit(EXIT_FAILURE);
-  }
-  setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    if ((fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    setsockopt(s, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
 
-  // Set it non-blocking
-  if (-1 == setnonblock(fd)) {
-    perror("echo server socket nonblock");
-    exit(EXIT_FAILURE);
-  }
+    // Set it non-blocking
+    if (-1 == setnonblock(fd)) {
+        perror("echo server socket nonblock");
+        exit(EXIT_FAILURE);
+    }
 
-  // Set it as unix socket
-  socket_un->sun_family = AF_INET;
-  //strcpy(socket_un->sun_path, sock_path);
+    // Set it as unix socket
+    socket_un->sun_family = AF_INET;
+    //strcpy(socket_un->sun_path, sock_path);
 
-  return fd;
+    return fd;
 }
 
 int server_init(struct sock_ev_serv* server, int max_queue) {
