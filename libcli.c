b04132340b47fb7690a2133cf4a3b18ec2bd2076
erik <erik@itancan.com>
2011-04-05 12:26:30 +0200

    Bugfixed orig print fcn so it does not add an extra newline, removed print2

--- libcli.c
+++ libcli.c
@@ -2083,9 +2083,7 @@ int cli_file(struct cli_def *cli, FILE *fh, int privilege, int mode)
     return CLI_OK;
 }
 
-// If newline != 0, print a trailing newline
-static void _print2(int newline, struct cli_def *cli, int print_mode,
-                    char *format, va_list ap)
+static void _print(struct cli_def *cli, int print_mode, char *format, va_list ap)
 {
     va_list aq;
     int n;
@@ -2114,6 +2112,9 @@ static void _print2(int newline, struct cli_def *cli, int print_mode,
         return;
 
     p = cli->buffer;
+
+    size_t slen = strlen(p);
+
     do
     {
         char *next = strchr(p, '\n');
@@ -2135,16 +2136,12 @@ static void _print2(int newline, struct cli_def *cli, int print_mode,
             if (cli->print_callback)
                 cli->print_callback(cli, p);
             else if (cli->client) {
-                if (newline) {
-                    fprintf(cli->client, "%s\r\n", p);
-                } else {
-                    fprintf(cli->client, "%s", p);
-                }
+                fprintf(cli->client, "%s\r\n", p);
             }
         }
 
         p = next;
-    } while (p);
+    } while (p && (p < cli->buffer + slen));
 
     if (p && *p)
     {
@@ -2152,12 +2149,6 @@ static void _print2(int newline, struct cli_def *cli, int print_mode,
         memmove(cli->buffer, p, strlen(p));
     }
     else *cli->buffer = 0;
-}
-
-static void _print(struct cli_def *cli, int print_mode,
-                   char *format, va_list ap)
-{
-    _print2(1, cli, print_mode, format, ap);
 }
 
 void cli_bufprint(struct cli_def *cli, char *format, ...)
@@ -2180,16 +2171,6 @@ void cli_print(struct cli_def *cli, char *format, ...)
 
     va_start(ap, format);
     _print(cli, PRINT_FILTERED, format, ap);
-    va_end(ap);
-}
-
-// Print without adding newline
-void cli_print_wonl(struct cli_def *cli, char *format, ...)
-{
-    va_list ap;
-
-    va_start(ap, format);
-    _print2(0, cli, PRINT_FILTERED, format, ap);
     va_end(ap);
 }
 
